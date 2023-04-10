8a732ffb47ad9ed6f789f84eb35f9bb96bc99c7a
Tony Khamphouy <tkham1@unh.newhaven.edu>
2023-04-10 12:49:47 -0400

    Initial Commit

---
+++ P9-Khamphouy/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  P9-Khamphouy
+//
+//  Created by Tony Khamphouy on 4/10/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
